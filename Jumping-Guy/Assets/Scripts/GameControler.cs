53df9abdeff2614d5568e08afee2a6fa6f9ad6ee
Val <valdo_jopa@hotmail.com>
2021-05-16 17:49:03 -0500

    animacion de correr y quieto
    animacion de run y idle del personaje completado

--- Jumping-Guy/Assets/Scripts/GameControler.cs
+++ Jumping-Guy/Assets/Scripts/GameControler.cs
@@ -14,6 +14,8 @@ public class GameControler : MonoBehaviour
     public enum GameState {Idle, Playing};
     public GameState gameState = GameState.Idle;
 
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,8 @@ public class GameControler : MonoBehaviour
         if (gameState == GameState.Idle && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
             gameState = GameState.Playing;
             uiIdle.SetActive(false);
+
+            player.SendMessage("UpdateState","PlayerRun");
         }
         //Juego en marcha
         else if (gameState == GameState.Playing){
---
+++ Jumping-Guy/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateState(string state = null){
+        if(state != null){
+            animator.Play(state);
+        }
+
+    }
+}
