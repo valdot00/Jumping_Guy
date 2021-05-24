5ff7e8e9a8e534dfd2b3c850e9b39d19603446ce
Val <valdo_jopa@hotmail.com>
2021-05-24 11:26:06 -0500

    animacion del enemigo
    agregando los sprites del enemigo

---
+++ Jumping-Guy/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour
+{
+    public float velocity = 2f;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = Vector2.left * velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
